34bb81725ccfde4cae9e2e0dcd0fc8a765f5b972
shiftdelete <angelito.tan23@gmail.com>
2024-10-29 01:38:49 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,21 @@
         <site-header/>
         <div>
             <div class="bg-[url('/images/home-bg.jpg')]" style=" background-repeat: no-repeat, repeat; background-size: cover;">
-                <div class="py-20 flex flex-col gap-3">
-                    <div class="text-white text-4xl font-bold">
-                        What Problem Do You Need Solved?
-                    </div>
-                    <div>
-                        <div class="w-2/4 border border-white rounded-full">
-                            <input type="text" class="rounded-full w-full text-white bg-black p-2" placeholder="Submit your problem here..."/>
+                <div class="py-40">
+                    <div class="flex flex-col gap-8 justify-center mb-5 items-center">
+                        <div class="text-white text-4xl font-bold">
+                            What Problem Do You Need Solved?
+                        </div>
+                        <div class="w-2/4">
+                            <div class="w-full border border-white rounded-full">
+                                <input type="text" class="rounded-full w-full text-white bg-black px-4 py-2" placeholder="Submit your problem here..."/>
+                            </div>
+                        </div>
+                        <div>
+                            <button class="border border-white px-5 py-3 text-white bg-darkgreen rounded-full"> Core Advisory Areas </button>
                         </div>
                     </div>
-                    <div>
-                        <button class="px-3 py-2 text-white bg-darkgreen rounded-full"> Core Advisory Areas </button>
-                    </div>
+           
                     <RightMenu/>
                 </div>
             </div>
